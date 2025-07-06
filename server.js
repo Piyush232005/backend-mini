1ab7d6584976c2c8681f695a15b2dae7ff816f00
Piyush <166606245+Piyush232005@users.noreply.github.com>
2025-07-06 22:53:15 +0530

    project complete

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json()); // for parsing application/json
 let notes = [];
 
 
-
+//  to add the notes 
 app.post("/notes", (req, res) => {
     console.log(req.body);
     notes.push(req.body);
@@ -16,8 +16,38 @@ app.post("/notes", (req, res) => {
         message: "Note added successfully",
         note: notes
     });
-    
 });
+
+// To Show the notes
+
+app.get("/notes", (req, res) => {
+    res.json(notes);
+});
+
+// To delete the notes
+app.delete("/notes/:index", (req, res) =>{
+    const index = req.params.index;
+    delete notes[index];
+    res.json({
+        message: "Note deleted successfully"
+    })
+})
+
+// To update the notes PATCH  /notes/:index =>{title, description}
+
+app.patch("/notes/:index", (req, res) => {
+    const index = req.params.index
+    const {title} = req.body;
+    notes[index].title = title;
+    res.json({
+        message: "Note updated successfully",
+        note: notes[index]
+    });
+})
+
+
+
+
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
